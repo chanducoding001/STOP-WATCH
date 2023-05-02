76d693adbf0bbf4f90f22bcb487c46f8e51f1a2c
chandrashekar <127927652+chanducoding001@users.noreply.github.com>
2023-05-02 13:00:07 +0530

    added project theory through comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// brought required elements witheir ids to do functionalities.
 let startBtn = document.getElementById('start');
 let stopBtn = document.getElementById('stop');
 let resetBtn = document.getElementById('reset');
@@ -5,18 +6,31 @@ let resetBtn = document.getElementById('reset');
 let hours = 00;
 let minutes = 00;
 let seconds = 00;
+// taken count as -1 initially as i am calling setInterval function every 10ms when count reaches to 100,
+// container shakes with addition of extra letter in the millisec count display
 let count = -1;
+// timer variable is a game changer here with that presence you,
+// cannot let start button do its action again when it is on. so that timer will not go crazy like increasing timer count
+//speed everytime you click on start button.
+
 let timer=false;
+// i am using setInterval inside start button only so for that i am taking interval null globally.
 let interval=null;
 
+// start button click event happening when that happening, start function address is referred
 startBtn.addEventListener("click",()=>{
+    // i am calling setInterval only when the timer is false i.e !timer to make start counter count only once when it is clicked
+    // and no further action until start or stop is clicked
     if(!timer){
         interval=setInterval(start,10)
     }
+    // making timer to true,you are disabling start counter until stop or reset is clicked
     timer=true;
 })
 function start(){
     count++;
+    // start counter put conditions with the time change to defferentiate with seconds,minutes,hours.
+
     if(count<10){
         document.getElementById("count").innerHTML="0"+count;
     }else{
@@ -53,13 +67,21 @@ function start(){
     
 }
 stopBtn.addEventListener("click",()=>{
+    // now making timer to false to enable start btn
     timer=false;
+    // disabling the interval functionality
     clearInterval(interval)});
 resetBtn.addEventListener("click",()=>{
-   // timer=false;
+    // below comment if you set to true after start button is enabled, reset button also have access to stop timer 
+    //but i am making the flow from start>>>>stop>>>>reset
+    // i.e after clicking start if you click only stop then reset is enabled. if not with timer is true, 
+    // reset also stops the start button functionality
+
+// timer=false;
    if(!timer){
     clearInterval(interval);
    }
+//    making count seconds minutes hours to their default values.
     count=-1;
     seconds=0;
     minutes=0;
